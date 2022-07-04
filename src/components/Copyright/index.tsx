099d2efcbc4beb67027f9e72ea761563c78cda1e
Gui-dev <adrianon10@gmail.com>
2022-07-04 03:36:46 -0300

    feat: add component Copyright

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './style'
+
+export const Copyright = () => {
+  return (
+    <View>
+      <Text style={styles.content}>Feito com muito ❤️</Text>
+    </View>
+  )
+}
---
+++ src/components/Copyright/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+// import { getBottomSpace } from 'react-native-iphone-x-helper'
+import { theme } from '../../theme'
+
+const styles = StyleSheet.create({
+  content: {
+    fontSize: 12,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_secondary
+  }
+})
+
+export default styles
