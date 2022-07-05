3f9ed7085cb9c68a9983ef4cae9b5c655c725787
Gui-dev <adrianon10@gmail.com>
2022-07-05 02:08:19 -0300

    feat: add component Load

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
+import 'react-native-gesture-handler'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Inter_400Regular, Inter_500Medium, useFonts } from '@expo-google-fonts/inter'
+
+import Widget from './src/components/Widget'
+import { Load } from './src/components/Load'
+import { theme } from './src/theme'
 
 export default function App () {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return <Load />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      {/* @ts-ignore */}
+      <Widget />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
